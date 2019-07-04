594ab0df4332d38c1354f942353a8518846361fa
Filthy Luis <notcarbide@users.noreply.github.com>
2019-07-04 00:28:11 +0000

    sh: build: Time and build log for make

--- build.sh
+++ build.sh
@@ -54,4 +54,4 @@ export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
     --program-suffix=-9.1 \
     --enable-default-pie
 
-make -j 4 profiledbootstrap
+time logsave build.log make -j 4 profiledbootstrap
